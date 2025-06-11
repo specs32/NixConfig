ec89e3074b031c62a3b91dc172302dcb5e1b76ce
specs32 <nicoandmomo@gmail.com>
2025-06-11 20:04:47 +0200

    boot and utils

--- boot.nix
+++ boot.nix
@@ -5,8 +5,11 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.extraModulePackages = [ config.boot.kernelPackages.zenpower ];
+  # boot.blacklistedKernelModules = [ "k10temp" ];
   boot.kernelModules = [
     "nct6775"
+    # "zenpower"
   ];
 
   boot.kernelParams = [
