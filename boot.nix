eafa7bb32c81548d77de4fe983c4c33683d1019c
specs32 <nicoandmomo@gmail.com>
2025-06-20 16:49:00 +0200

    jo

--- boot.nix
+++ boot.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ pkgs, lib, config, ... }:
 
 {
   system.nixos.label = "Bob";
@@ -6,28 +6,28 @@
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    initrd.enable = true;
-    initrd.verbose = false;
-    initrd.systemd.enable = true;
-    initrd.availableKernelModules = [ "amdgpu" ];
-    initrd.kernelModules          = [ "amdgpu" ];
+#    initrd.enable = true;
+#    initrd.verbose = false;
+#    initrd.systemd.enable = true;
+#    initrd.availableKernelModules = [ "amdgpu" ];
+#    initrd.kernelModules          = [ "amdgpu" ];
     kernelPackages = pkgs.linuxPackages_latest;
     kernelModules = [ "nct6775" ];
-    consoleLogLevel = 3;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "udev.log_priority=3"
-      "rd.systemd.show_status=auto"
-      ];
+#    consoleLogLevel = 3;
+#    kernelParams = [
+#      "quiet"
+#      "splash"
+#      "boot.shell_on_fail"
+#      "udev.log_priority=3"
+#      "rd.systemd.show_status=auto"
+#      ];
 
-    loader.timeout = 0;
+#    loader.timeout = 0;
 
-    plymouth = {
-      enable = true;
-      font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
-      themePackages = [ pkgs.nixos-bgrt-plymouth ];
-    };
+#    plymouth = {
+#      enable = true;
+#      font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
+#      themePackages = [ pkgs.nixos-bgrt-plymouth ];
+#    };
   };
 }
